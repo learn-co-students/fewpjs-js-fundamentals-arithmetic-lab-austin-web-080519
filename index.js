dedc87cb7e1f44df0088c3ccbc8c5f6e3d4918ff
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-22 18:58:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+var num1 = 31
+var num2 = 2
+
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random() + 1);
+
+var mod = 44 % 10;
+
+var max = Math.max(1,5,10,20)
