47cd1330b4ecaf821e34af511fd3194bf77a3528
Cody <lawdermiltcody@gmail.com>
2019-09-22 21:43:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() + 1);
+
+let mod = 44 % 10; 
+
+let max = Math.max(1, 3, 5, 6, 7, 20);
